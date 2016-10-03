306e2b4ca8c7e500b240e0cd557eebe48316d4cb
Danny Peck <danny.peck@inin.com>
2016-10-03 14:02:18 -0400

    added correct call to datastore for games

--- app/routes/games.js
+++ app/routes/games.js
@@ -3,11 +3,6 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   // returns game data
   model() {
-    var gamesPromise = this.get('store').findAll('game');
-    
-    gamesPromise.then((games) => {
-      console.log(games);
-      return games;
-    });
+    return this.get('store').findAll('game');
   }
 });
